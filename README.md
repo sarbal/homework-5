846c2d76efa1b97f8151e176556431deeb3af1ce
Pavlin Poličar <pavlin.g.p@gmail.com>
2020-12-20 18:53:41 +0100

    Add homework 5

---
+++ README.md
@@ -0,0 +1,39 @@
+# Homework 5
+
+## Submission and Grading
+
+To complete the homework, you need to commit and push your images/notebook/code to your Github repository. Please read and follow the notebook instructions carefully. You do not need to submit anything else. We will open assignments on Ucilnica, but these are there only for your convenince, so the due dates are visible in your Ucilnica calendar.
+
+For grading, we automatically generate a report for every student, so we never actually go through your code and notebooks. Our report generator automatically goes through your submission and pulls out the relevant bits.
+
+There are three types of exercises/answers within each homework:
+
+1. *Coding exercises* require you to implement an algorithm. We will always include Python stubs for the functions you need to implement in a separate file (most often `helper_functions.py`). Please read the function docstrings for expected parameter and return types. Coding problems will be automatically graded with unit tests.
+
+2. *Image answers* require you to generate an image and save the plot to a corresponding file in `<img>.png`. Please ensure that your images have the exact same name as required in the instructions.
+
+3. *Variable answers* require you to write down your answers into variables. Please be careful that the variables have the exact same name as in the instructions. We will always provide a stub in the notebook. We don't actually run your entire notebooks, we evaluate only the variable we are grading. This means that your variables should be set explicitly. For instance
+
+    ```python
+    x = 5 / 3
+    answer_var = x
+    ```
+
+    will not work and will receive zero points. All answer variables should be set explicitly e.g. `answer_var = 1.6666` in for this simple example. Please ensure there are no syntax errors in your notebook.
+
+We will automatically fetch your solutions at the deadline and these submissions will be graded by default. **If you submit after the deadline -- using your late days -- please notify Pavlin on our Slack channel**, and we will re-evaluate your submission.
+
+## Environment instructions
+
+You will need Python 3.8 or higher. You will need to install `biopython` for accessing NCBI and `matplotlib` for plotting. You will also need `jupyterlab` to open and run the notebook. You will also need `numpy`, `scipy`, `pandas`, `scanpy`, and `seaborn`. You can install everything necessary by running
+```
+pip install biopython matplotlib jupyterlab pandas scanpy
+```
+Please do not use any other libraries, because they will not be installed in the automatic grader environment and will fail, resulting in zero points. If you think some other library absolutely needs to be included, please reach out on Slack and we will discuss it there.
+
+You can start the notebook by running
+```
+jupyter lab
+```
+and a browser window should pop open, or you can manually navigate to http://localhost:8888/ in your browser.
+
---
+++ helper_functions.py
@@ -0,0 +1,165 @@
+from collections import namedtuple
+from typing import Dict, List, Tuple
+import pandas as pd
+from Bio.SeqRecord import SeqRecord
+
+GffEntry = namedtuple(
+    "GffEntry",
+    [
+        "seqname",
+        "source",
+        "feature",
+        "start",
+        "end",
+        "score",
+        "strand",
+        "frame",
+        "attribute",
+    ],
+)
+
+
+GeneDict = Dict[str, GffEntry]
+
+
+def read_gff(fname: str) -> Dict[str, GffEntry]:
+    gene_dict = {}
+
+    with open(fname) as f:
+        for line in f:
+            if line.startswith("#"):  # Comments start with '#' character
+                continue
+
+            parts = line.split("\t")
+            parts = [p.strip() for p in parts]
+
+            # Convert start and stop to ints
+            start_idx = GffEntry._fields.index("start")
+            parts[start_idx] = int(parts[start_idx]) - 1  # GFFs count from 1..
+            stop_idx = GffEntry._fields.index("end")
+            parts[stop_idx] = int(parts[stop_idx]) - 1  # GFFs count from 1..
+
+            # Split the attributes
+            attr_index = GffEntry._fields.index("attribute")
+            attributes = {}
+            for attr in parts[attr_index].split(";"):
+                attr = attr.strip()
+                k, v = attr.split("=")
+                attributes[k] = v
+            parts[attr_index] = attributes
+
+            entry = GffEntry(*parts)
+
+            gene_dict[entry.attribute["gene_name"]] = entry
+
+    return gene_dict
+
+
+def split_read(read: str) -> Tuple[str, str]:
+    """Split a given read into its barcode and DNA sequence. The reads are
+    already in DNA format, so no additional work will have to be done. This
+    function needs only to take the read, and split it into the cell barcode,
+    the primer, and the DNA sequence. The primer is not important, so we discard
+    that.
+
+    The first 12 bases correspond to the cell barcode.
+    The next 24 bases corresond to the oligo-dT primer. (discard this)
+    The reamining bases corresond to the actual DNA of interest.
+
+    Parameters
+    ----------
+    read: str
+
+    Returns
+    -------
+    str: cell_barcode
+    str: mRNA sequence
+
+    """
+    raise NotImplementedError()
+
+
+def map_read_to_gene(read: str, ref_seq: str, genes: GeneDict) -> Tuple[str, float]:
+    """Map a given read to a gene with a confidence score using Hamming distance.
+
+    Parameters
+    ----------
+    read: str
+        The DNA sequence to be aligned to the reference sequence. This should
+        NOT include the cell barcode or the oligo-dT primer.
+    ref_seq: str
+        The reference sequence that the read should be aligned against.
+    genes: GeneDict
+
+    Returns
+    -------
+    gene: str
+        The name of the gene (using the keys of the `genes` parameter, which the
+        read maps to best. If the best alignment maps to a region that is not a
+        gene, the function should return `None`.
+    similarity: float
+        The similarity of the aligned read. This is computed by taking the
+        Hamming distance between the aligned read and the reference sequence.
+        E.g. catac and cat-x will have similarity 3/5=0.6.
+
+
+    """
+    raise NotImplementedError()
+
+
+def generate_count_matrix(
+    reads: str,
+    ref_seq: str,
+    genes: GeneDict,
+    similarity_threshold: float
+) -> pd.DataFrame:
+    """
+
+    Parameters
+    ----------
+    reads: List[str]
+        The list of all reads that will be aligned.
+    ref_seq: str
+        The reference sequence that the read should be aligned against.
+    genes: GeneDict
+    similarity_threshold: float
+
+    Returns
+    -------
+    count_table: pd.DataFrame
+        The count table should be an N x G matrix where N is the number of
+        unique cell barcodes in the reads and G is the number of genes in
+        `genes`. The dataframe columns should be to a list of strings
+        corrsponding to genes and the dataframe index should be a list of
+        strings corresponding to cell barcodes. Each cell in the matrix should
+        indicate the number of times a read mapped to a gene in that particular
+        cell.
+
+    """
+    raise NotImplementedError()
+
+
+def filter_matrix(
+    count_matrix: pd.DataFrame,
+    min_counts_per_cell: int,
+    min_counts_per_gene: int,
+) -> pd.DataFrame:
+    """Filter a matrix by cell counts and gene counts.
+    The cell count is the total number of molecules sequenced for a particular
+    cell. The gene count is the total number of molecules sequenced that
+    correspond to a particular gene. Filtering statistics should be computed on
+    the original matrix. E.g. if you filter out the genes first, the filtered
+    gene molecules should still count towards the cell counts.
+
+    Parameters
+    ----------
+    count_matrix: pd.DataFrame
+    min_counts_per_cell: float
+    min_counts_per_gene: float
+
+    Returns
+    -------
+    filtered_count_matrix: pd.DataFrame
+
+    """
+    raise NotImplementedError()
